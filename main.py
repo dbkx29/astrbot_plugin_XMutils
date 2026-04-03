8b5746476f236586d18efb36c3bac34ccb06891c
dbkx29 <1733835863@qq.com>
2026-04-03 20:07:05 +0800

    重构xmhelp插件，删除旧实现并添加新功能，更新版本号至v0.0.3

--- main.py
+++ main.py
@@ -22,3 +22,32 @@ class MyPlugin(Star):
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+
+@register("xmhelp", "XMUtils", "筱鸣壹形功能帮助插件", "1.0.0")
+class XMHelpPlugin(Star):
+    def __init__(self, context: Context):
+        super().__init__(context)
+
+    async def initialize(self):
+        """插件初始化方法"""
+
+    @filter.command("xmhelp")
+    async def xmhelp(self, event: AstrMessageEvent):
+        """呼出筱鸣壹形帮助信息"""
+        help_msg = (
+            "-----筱鸣壹形β食用说明-----\n"
+            "[xmds]\ndeepseek对话（25.2.6new）\n"
+            "[xmhelp]\n用于呼出此说明\n"
+            "[xmjrrp]\n用于查看今日人品\n"
+            "[xmsetu]\n用于获取随机涩图\n"
+            "[b站视频链接]\nb站视频解析\n"
+            "[摸/膜/吃/捅/@昵称]\n对指定用户做出指定行为\n"
+            "[xmeat]\n觅食成贤\n"
+            "[xmdice (\d+)d(\d+)]\n骰子\n"
+            "[xmtp]\n台风信息\n"
+            "---祝您使用愉快2023/8/2---"
+        )
+        yield event.plain_result(help_msg)
+
+    async def terminate(self):
+        """插件销毁方法"""
